4f9628e905136737319c2c6b3ecdc508c04f8d3b
Brenda Ribon <63269769+brendaribon@users.noreply.github.com>
2020-09-23 12:44:19 -0300

    Update login.page.dart

---
+++ root_nerd/lib/login.page.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+class LoginPage extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text( "Login",)
+      ),
+      );
+  }
+}
