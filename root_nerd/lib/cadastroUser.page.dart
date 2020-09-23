89cf91369e8895275b8484321542a7b82db8f0e1
Fantasia <vinicius.fantasia10@gmail.com>
2020-09-23 12:57:08 -0300

    cadastro usuario 0.1

---
+++ root_nerd/lib/cadastroUser.page.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+class CadastroUserPAge extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+        "Cadastro de Usuário",
+      )),
+    );
+  }
+}
