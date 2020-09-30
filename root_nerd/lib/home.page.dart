7b14265cfb44e8ecd4a7d0a090aa6e95eccd346e
Fantasia <vinicius.fantasia10@gmail.com>
2020-09-30 05:56:06 -0300

    home v0.1

---
+++ root_nerd/lib/home.page.dart
@@ -0,0 +1,35 @@
+// 1. IMPORTAR AS BIBLIOTECAS QUE UTILIZAREI NA TELA
+// 2. CRIAR A CLASSE QUE HERDE DE **STATELESSWIDGET** OU STATEFULWIDGET
+// 3. IMPLEMENTAR O MÉTODO BUILD DA CLASSE
+// 4. RETORNAR O WIDGET QUE DESEJA MOSTRAR NA TELA DENTRO DO MÉTODO BUILD.
+
+import 'package:flutter/material.dart';
+import 'package:root_nerd/itemnerd.widget.dart';
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("RootNerd"),
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          ItemNerd(),
+          ItemNerd(),
+          ItemNerd(),
+          ItemNerd(),
+          ItemNerd(),
+          ItemNerd(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        tooltip: "Novo Item Nerd",
+        onPressed: () {
+          Navigator.of(context).pushNamed('/cadastro');
+        },
+      ),
+    );
+  }
+}
--- root_nerd/lib/main.dart
+++ root_nerd/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:root_nerd/login.page.dart';
 import 'package:root_nerd/cadastroUser.page.dart';
+import 'package:root_nerd/home.page.dart';
 
 void main() {
   runApp(MyApp()); // Qual é a classe que será carregada (exibida).
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => LoginPage(),
         '/cadastroUser': (context) => CadastroUserPage(),
+        '/home': (context) => HomePage(),
       },
       initialRoute: '/',
       debugShowCheckedModeBanner: false,
