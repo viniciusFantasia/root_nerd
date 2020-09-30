52fbd26b2288aaa1277e0a5824632c0b4c4e5a9c
Fantasia <vinicius.fantasia10@gmail.com>
2020-09-30 05:56:34 -0300

    itemnerd v0.1

---
+++ root_nerd/lib/itemnerd.widget.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class ItemNerd extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed("/detalhe");
+      },
+      // onLongPress: () {
+      //   Navigator.of(context).pushNamed("/cadastro");
+      // },
+      child: Container(
+        margin: EdgeInsets.fromLTRB(12, 12, 12, 0),
+        padding: EdgeInsets.all(8),
+        //color: Colors.white,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(0, 0, 0, 0.1),
+                offset: Offset(4, 4),
+                blurRadius: 12,
+              ),
+            ]),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text('One Piece - Ep.872'),
+                Text('#anime'),
+              ],
+            ),
+            Image.network(
+              'https://via.placeholder.com/700x250.png?text=Image1',
+            ),
+            Text(
+                'Que incrível a tecnologia usada nesse episódio, animação fluida e coreografia de luta impecável, amei demais, cada vez melhor!'),
+            Text('https://www.crunchyroll.com/pt-br'),
+            Divider(),
+            GestureDetector(
+              // onTap: () => Navigator.of(context).pushNamed('/cadastro'),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundImage: NetworkImage(
+                        'https://www.diariodepernambuco.com.br/static/app/noticia_127983242361/2019/12/03/812540/20191203093743682044o.jpg'),
+                  ),
+                  SizedBox(
+                    width: 6,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "@Otaku",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        "30/09/2020",
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 12,
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Column(
+                        children: [Icon(Icons.add), Text('1')],
+                      ),
+                      Column(
+                        children: [Icon(Icons.add), Text('2')],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
