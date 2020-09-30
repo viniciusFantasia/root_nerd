d6d2cd6ff258d32f7f46f68325344ae013d1bd58
Brenda Ribon <63269769+brendaribon@users.noreply.github.com>
2020-09-30 11:47:31 -0300

    Update login.page.dart

--- root_nerd/lib/login.page.dart
+++ root_nerd/lib/login.page.dart
@@ -1,30 +1,95 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  var _formKey = GlobalKey<FormState>();
 
+  @override
   Widget build(BuildContext context) {
+    double larguraTela = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text( "Login",),
-      ),
-      body: Container(
-         padding: EdgeInsets.all(10),
-        color: Colors.black,
-        child: Column( 
-          
-          children: [
-            CircleAvatar(
-              radius: 100,
-              backgroundImage: AssetImage('../img/home.png'),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+           width: larguraTela > 700 ? 400 : larguraTela,
+            padding: EdgeInsets.all(10),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                CircleAvatar(
+                  radius: 100,
+                  backgroundImage: AssetImage('../img/home.png'),
             ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Nome",
-                ),            
-              ), 
-              ],
+                  SizedBox(height: 20),
+                  TextFormField(
+                    validator: (value) =>
+                        value.isEmpty ? "Campo obrigatório" : null,
+                      decoration: InputDecoration(
+                        labelStyle: TextStyle(color: Colors.grey),
+                        labelText: "E-mail",
+                      border: OutlineInputBorder(),
+                    ),
+                      style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 8),
+                  TextFormField(
+                    validator: (value) =>
+                        value.isEmpty ? "Campo obrigatório" : null,
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.grey),
+                      labelText: "Senha",
+                      border: OutlineInputBorder(),
+                    ),
+                    style: TextStyle(color: Colors.white),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 8),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate())
+                          Navigator.of(context).pushNamed('/home');
+                      },
+                      child: Text(
+                        "Entrar",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text("Não tem cadastro?",
+                      style: TextStyle(
+                          color: Colors.white,
+                        ),),
+                      FlatButton(
+                        padding: EdgeInsets.all(0),
+                        onPressed: () {
+                          Navigator.of(context).pushNamed('/registro');
+                        },
+                        child: Text(
+                          "Clique aqui.",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
+          ),
+        ),
       ),
-      );
+    );
   }
 }
