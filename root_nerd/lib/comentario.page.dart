05c7347add20d3c396de4ba35f0f52f1cdb234e3
Fantasia <vinicius.fantasia10@gmail.com>
2020-10-07 17:49:11 -0300

    página comentários e detalhe

--- root_nerd/lib/comentario.page.dart
+++ root_nerd/lib/comentario.page.dart
@@ -2,29 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:root_nerd/itemcomentario.widget.dart';
 
 class ComentarioPage extends StatelessWidget {
+  var formKey = GlobalKey<FormState>();
+
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("RootNerd"),
+        title: new Text(
+          "Comentários",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Color(0xFF99e265),
+        centerTitle: true,
       ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          Text('Comentários'),
-          ItemComentario(),
-          ItemComentario(),
-          ItemComentario(),
-          ItemComentario(),
-          ItemComentario(),
-          ItemComentario(),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        tooltip: "Novo Comentário",
-        onPressed: () {
-          Navigator.of(context).pushNamed('/cadastroComentario');
-        },
+      body: Form(
+        key: formKey,
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  ItemComentario(),
+                  ItemComentario(),
+                  ItemComentario(),
+                  ItemComentario(),
+                  ItemComentario(),
+                  ItemComentario(),
+                  ItemComentario(),
+                  ItemComentario(),
+                  ItemComentario(),
+                  ItemComentario(),
+                  ItemComentario(),
+                  ItemComentario(),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Nome",
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
+                    ),
+                    validator: (value) =>
+                        value.isEmpty ? 'Campo obrigatório' : null,
+                    autovalidate: false,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.keyboard_arrow_right),
+                    onPressed: () {
+                      if (formKey.currentState.validate()) {
+                        // TODO: Salvar os dados no banco de dados (Firestore)
+                        // TODO: Mostrar uma mensagem de sucesso.
+                        Navigator.of(context).pop();
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- root_nerd/lib/detalhe.page.dart
+++ root_nerd/lib/detalhe.page.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
 
 class DetalhePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Item Nerd"),
+        title: Text(
+          "Item Nerd",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Color(0xFF99e265),
+        centerTitle: true,
       ),
       body: Column(
         children: [
@@ -16,58 +22,48 @@ class DetalhePage extends StatelessWidget {
                 'https://via.placeholder.com/700x400.png?text=Image1',
                 fit: BoxFit.cover),
           ),
-          Text('One Piece - Ep.872',
-              style: TextStyle(
-                color: Colors.white,
-              )),
+          Text('One Piece - Ep.872'),
           Row(
             children: [
               Column(
                 children: [
-                  Text('#anime',
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
-                  Text('www.siteparaver.com',
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
+                  Text('#anime'),
+                  Text('www.siteparaver.com'),
                 ],
               ),
               Column(
                 children: [
-                  Icon(Icons.add, color: Colors.white),
-                  Text('1',
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
+                  Icon(Icons.stars),
+                  Text('1'),
                 ],
               ),
-              Column(
-                children: [
-                  Icon(Icons.add, color: Colors.white),
-                  Text('1',
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
-                ],
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/comentario');
+                },
+                child: Column(
+                  children: [
+                    Icon(Icons.add_comment),
+                    Text('1'),
+                  ],
+                ),
               ),
             ],
           ),
           Text(
-              'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
-              style: TextStyle(
-                color: Colors.white,
-              )),
-          Text('Avaliações de quem assistiu:',
-              style: TextStyle(
-                color: Colors.white,
-              )),
-          Text('rating interativo',
-              style: TextStyle(
-                color: Colors.white,
-              )),
-          Icon(Icons.add)
+              'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'),
+          Text('Avaliações de quem assistiu:'),
+          SmoothStarRating(
+              allowHalfRating: false,
+              onRated: (v) {},
+              starCount: 5,
+              rating: 3,
+              defaultIconData: Icons.star_border,
+              size: 40.0,
+              isReadOnly: false,
+              color: Color(0xFF99e265),
+              borderColor: Color(0xFF99e265),
+              spacing: 0.0)
         ],
       ),
     );
