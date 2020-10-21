dc925ab65cc05f0e09602d7d11a4ef8fa318fb22
Fantasia <vinicius.fantasia10@gmail.com>
2020-10-21 09:58:56 -0300

    checklist das #

--- root_nerd/lib/home.page.dart
+++ root_nerd/lib/home.page.dart
@@ -6,7 +6,17 @@
 import 'package:flutter/material.dart';
 import 'package:root_nerd/itemnerd.widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool anime = false;
+  bool filme = false;
+  bool hq = false;
+  bool desenho = false;
+  bool manga = false;
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
@@ -20,38 +30,106 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: Drawer(
-          child: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          DrawerHeader(
-            child: Text('RootNerd',
+          child: Container(
+        margin: EdgeInsets.all(10),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            Text('RootNerd',
                 style: TextStyle(
                   color: Color(0xFF99e265),
                   fontSize: 20,
                 )),
-          ),
-          // CheckboxListTile(
-          //     title: Text('#anime'),
-          //     value: ,
-          //     onChanged: (bool value) {
-          //       setState(() {
-          //       });
-          //     }),
-          ListTile(
-            title: FlatButton(
-                child: Text(
-                  "Editar Perfil",
-                  style: TextStyle(
-                    fontSize: 15,
-                  ),
-                ),
-                onPressed: () {
-                  //TODO: Salvar os dados do banco de dados (Firestone)
-                  //TODO: Mostrar uma mensagem de sucesso.
-                  Navigator.of(context).pushNamed('/perfil');
+            SizedBox(
+              height: 30,
+            ),
+            Text('Filtro',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            CheckboxListTile(
+                title: Text('#anime'),
+                value: anime,
+                onChanged: (value) {
+                  setState(() {
+                    anime = value;
+                  });
                 }),
-          ),
-        ],
+            SizedBox(
+              height: 10,
+            ),
+            CheckboxListTile(
+                title: Text('#filme'),
+                value: filme,
+                onChanged: (value) {
+                  setState(() {
+                    filme = value;
+                  });
+                }),
+            SizedBox(
+              height: 10,
+            ),
+            CheckboxListTile(
+                title: Text('#HQ'),
+                value: hq,
+                onChanged: (value) {
+                  setState(() {
+                    hq = value;
+                  });
+                }),
+            SizedBox(
+              height: 10,
+            ),
+            CheckboxListTile(
+                title: Text('#desenho'),
+                value: desenho,
+                onChanged: (value) {
+                  setState(() {
+                    desenho = value;
+                  });
+                }),
+            SizedBox(
+              height: 10,
+            ),
+            CheckboxListTile(
+                title: Text('#desenho'),
+                value: manga,
+                onChanged: (value) {
+                  setState(() {
+                    manga = value;
+                  });
+                }),
+            SizedBox(
+              height: 10,
+            ),
+            ListTile(
+              title: FlatButton(
+                  child: Row(
+                    children: [
+                      Icon(Icons.person),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        "Editar Perfil",
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    //TODO: Salvar os dados do banco de dados (Firestone)
+                    //TODO: Mostrar uma mensagem de sucesso.
+                    Navigator.of(context).pushNamed('/perfil');
+                  }),
+            ),
+          ],
+        ),
       )),
       body: ListView(
         scrollDirection: Axis.vertical,
